2c842dfe676a9cfc4a328f34e2ee2e95757f561c
Your Name <simon.be47@gmail.com>
2022-11-02 18:13:50 +0100

    modification dans le 'readme.md', ajout d'un fichier 'site.webmanifest', enrichissement du dossier et du design pattern sass, modification des 'settings.json', ajout de mixin et variables sass, création de 3 sections en html et leurs contenu.

--- db/marche_ou_cepes.sql
+++ db/marche_ou_cepes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le : lun. 31 oct. 2022 à 18:35
+-- Généré le : lun. 31 oct. 2022 à 20:03
 -- Version du serveur : 5.7.24
 -- Version de PHP : 8.0.1
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `marche_ou_cepes`
 --
+CREATE DATABASE IF NOT EXISTS `marche_ou_cepes` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `marche_ou_cepes`;
 
 -- --------------------------------------------------------
 
@@ -29,21 +31,19 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `admin` (
   `id` int(11) NOT NULL,
-  `login` varchar(255) NOT NULL DEFAULT 'admin',
-  `mdp` varchar(255) NOT NULL DEFAULT 'admin'
+  `login` varchar(255) NOT NULL,
+  `mdp` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `a partager`
+-- Structure de la table `categories`
 --
 
-CREATE TABLE `a partager` (
+CREATE TABLE `categories` (
   `id` int(11) NOT NULL,
-  `label` varchar(255) NOT NULL DEFAULT 'a partager',
-  `titre` varchar(255) NOT NULL,
-  `description` text NOT NULL
+  `nom` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -61,40 +61,15 @@ CREATE TABLE `configuration` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `desserts`
---
-
-CREATE TABLE `desserts` (
-  `id` int(11) NOT NULL,
-  `label` varchar(255) NOT NULL DEFAULT 'dessert',
-  `titre` varchar(255) NOT NULL,
-  `description` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `entr??es`
---
-
-CREATE TABLE `entr??es` (
-  `id` int(11) NOT NULL,
-  `label` varchar(255) NOT NULL DEFAULT 'Entrée',
-  `titre` varchar(255) NOT NULL,
-  `description` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `plats`
 --
 
 CREATE TABLE `plats` (
   `id` int(11) NOT NULL,
-  `label` varchar(255) NOT NULL DEFAULT 'plat',
+  `label` varchar(255) NOT NULL,
   `titre` varchar(255) NOT NULL,
-  `description` text NOT NULL
+  `description` text NOT NULL,
+  `categorie` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -108,49 +83,27 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
 --
--- Index pour la table `a partager`
+-- Index pour la table `categories`
 --
-ALTER TABLE `a partager`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `desserts`
---
-ALTER TABLE `desserts`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `entr??es`
---
-ALTER TABLE `entr??es`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`);
 
 --
 -- Index pour la table `plats`
 --
 ALTER TABLE `plats`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `categorie` (`categorie`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT pour la table `a partager`
+-- AUTO_INCREMENT pour la table `categories`
 --
-ALTER TABLE `a partager`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `desserts`
---
-ALTER TABLE `desserts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `entr??es`
---
-ALTER TABLE `entr??es`
+ALTER TABLE `categories`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
@@ -158,6 +111,16 @@ ALTER TABLE `entr??es`
 --
 ALTER TABLE `plats`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `categories`
+--
+ALTER TABLE `categories`
+  ADD CONSTRAINT `categories_ibfk_1` FOREIGN KEY (`id`) REFERENCES `plats` (`categorie`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ readme.md
@@ -0,0 +1,50 @@
+# Site vitrine restaurateur Marche ou cèpes :  
+
+---
+
+## Brief client :  
+
+
+---
+
+## Détails des pages :  
+
+**Une page utilisateur en mode mobile/tablette/desktop:**  
+* En-tête :
+    * Logo
+    * Menu de navigation :
+        * Accueil
+        * Menus
+        * Formulaire de contact
+        * Adresse du restaurant
+* corps de page :
+    section acceuil :
+        * Logo
+        * Biographie/description du restaurant
+        * Adresse du restaurant
+    section menu :
+        * Un menu séparé en quatre "section" : 
+            * Entrées
+            * Plats
+            * A partager
+            * Desserts
+    section formulaire de contact :
+        * Champs prénom
+        * Champs nom
+        * Champs n°telephopne
+        * Champs de texte 
+        * bouton d'envois de formulaire
+    section map interractive :
+        * Une carte interractive positionnée (marqueur) sur l'adresse du restaurant
+        * L'utilisateur peut zoomer et dé-zoomer sur la map
+* Pied de page :
+    * Pour le mobile :
+        * Icônes :
+            *
+            *
+    * Mentions légales
+    * CGU
+
+**Une page contenant un formulaire de connexion pour le back-office/administrateur**  
+
+**Une page de gestion/édition de contenu coté administrateur**  
