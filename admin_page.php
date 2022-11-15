28c3b0bd936d45fa154181e9fab6b25e9901be5a
Azogagh Mohamed <mohamed@MacBook-Air-de-Azogagh.local>
2022-11-15 17:03:13 +0100

    modif

--- admin_page.php
+++ admin_page.php
@@ -1,12 +1,13 @@
 <?php
-    session_start();
-    if ($_SESSION['connected'] != true) {
-        header('location:admin_connexion.php');
-    }
+session_start();
+if ($_SESSION['connected'] != true) {
+    header('location:admin_connexion.php');
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -14,10 +15,15 @@
     <link rel="stylesheet" href="assets/css/styles.min.css">
     <title>Espace Administrateur</title>
 </head>
+
 <body class="body">
-        <h1>Espace Administrateur</h1>
+    <h1>Bienvenue sur la page Administrateur</h1>
     <div class="log-out">
-    <a class="btn-logout" href="admin_deconnexion.php">Se déconnecter</a>
+        <a class="btn-logout" href="admin_deconnexion.php">Se déconnecter</a>
+    </div><br><br>
+    <div>
+        <p>Sur cette page il vous est possible de voir les contenus modifiables de votre site et d'y apportées des modifications</p>
     </div>
 </body>
+
 </html>
