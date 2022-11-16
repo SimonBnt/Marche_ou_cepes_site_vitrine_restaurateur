d5d1c78fdd8eebaf25dbb356f3c64c08084dfd72
Your Name <simon.be47@gmail.com>
2022-11-16 17:06:14 +0100

    fix frontend + remove burger menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,18 @@
 // ---- Menu Burger script ---- //
 
-let burgerMenu = document.getElementById("burgerMenu")
-let openBtn = document.getElementById("openBtn")
-let closeBtn = document.getElementById("closeBtn")
-let burgerMenuContainer = document.getElementById("burgerMenu_container")
-let navbar = document.getElementById("navbar")
+// let burgerMenu = document.getElementById("burgerMenu")
+// let openBtn = document.getElementById("openBtn")
+// let closeBtn = document.getElementById("closeBtn")
+// let burgerMenuContainer = document.getElementById("burgerMenu_container")
+// let navbar = document.getElementById("navbar")
 
-openBtn.onclick = openBurgerMenu
-closeBtn.onclick = closeBurgerMenu
+// openBtn.onclick = openBurgerMenu
+// closeBtn.onclick = closeBurgerMenu
 
-function openBurgerMenu() {
-    burgerMenu.classList.add("active");
-}
+// function openBurgerMenu() {
+//     burgerMenu.classList.add("active");
+// }
   
-function closeBurgerMenu() {
-    burgerMenu.classList.remove("active");
-}
+// function closeBurgerMenu() {
+//     burgerMenu.classList.remove("active");
+// }
